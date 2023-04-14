a887ba7e86061887e72e0f35403e14a76684fff2
Gonzalez-Ignacio <ignaciogonzalez2306@gmail.com>
2023-04-14 02:37:08 -0300

    Presentacion - Contacto
    Agregamos todo Presentacion.
    
    Ademas, corregimos boton enviar en Contactos

--- contacto/contacto.js
+++ contacto/contacto.js
@@ -1,9 +1,9 @@
-const btn = document.getElementsByClassName('boton');
-boton.addEventListener("click",validarDatos);
+// const btn = document.getElementsByClassName('boton');
+// boton.addEventListener("click",validarDatos);
 
 
-function validarDatos(){
-    alert("tocaste el boton");
+// function validarDatos(){
+//     alert("tocaste el boton");
     /*const email = document.getElementById("inputEmail").value;
     if (/^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|es)+$/.test(email)){
         let aviso = "";
@@ -12,4 +12,17 @@ function validarDatos(){
         let aviso = "Ingrese un email valido";
         document.getElementById("validacionEmail").innerHTML= aviso;
        }*/
-}
+// }
+
+
+const boton_enviar = document.getElementById("boton_enviar");
+boton_enviar.addEventListener("click", function(){
+    const email = document.getElementById("emailInput").value;
+    if (/^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|es)+$/.test(email)){
+        let aviso = "";
+        document.getElementById("validacionEmail").innerHTML= aviso;
+       } else {
+        let aviso = "Ingrese un email valido";
+        document.getElementById("validacionEmail").innerHTML= aviso;
+       }
+})
