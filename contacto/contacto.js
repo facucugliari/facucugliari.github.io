c6bb5fccc172e3e9890f5f1a1debc2fc8672651b
facucugliari <67327874+facucugliari@users.noreply.github.com>
2023-04-22 20:59:35 -0300

    commit final
    creacion github.io

--- contacto/contacto.js
+++ contacto/contacto.js
@@ -1,19 +1,3 @@
-// const btn = document.getElementsByClassName('boton');
-// boton.addEventListener("click",validarDatos);
-
-
-// function validarDatos(){
-//     alert("tocaste el boton");
-    /*const email = document.getElementById("inputEmail").value;
-    if (/^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|es)+$/.test(email)){
-        let aviso = "";
-        document.getElementById("validacionEmail").innerHTML= aviso;
-       } else {
-        let aviso = "Ingrese un email valido";
-        document.getElementById("validacionEmail").innerHTML= aviso;
-       }*/
-// }
-
 
 const boton_enviar = document.getElementById("boton_enviar");
 boton_enviar.addEventListener("click", function(){
